9b9652c4fe3d707f9ae17b4a1b7049b58dcf532e
DaniYDevs <danidzm18@gmail.com>
2025-09-25 00:04:03 +0200

    Amplied functionalities Fraction issue #7

--- src/main/java/es/upm/miw/devops/code/Fraction.java
+++ src/main/java/es/upm/miw/devops/code/Fraction.java
@@ -33,6 +33,35 @@ public class Fraction {
         return (double) numerator / denominator;
     }
 
+    public boolean isProper() {
+        return Math.abs(numerator) < Math.abs(denominator);
+    }
+
+    public boolean isImproper() {
+        return Math.abs(numerator) >= Math.abs(denominator);
+    }
+
+    public boolean isEquivalent(Fraction other) {
+        return this.numerator * other.denominator == this.denominator * other.numerator;
+    }
+
+    public Fraction add(Fraction other) {
+        int num = this.numerator * other.denominator + other.numerator * this.denominator;
+        int den = this.denominator * other.denominator;
+        return new Fraction(num, den);
+    }
+
+    public Fraction multiply(Fraction other) {
+        return new Fraction(this.numerator * other.numerator, this.denominator * other.denominator);
+    }
+
+    public Fraction divide(Fraction other) {
+        if (other.numerator == 0) {
+            throw new ArithmeticException("division by zero");
+        }
+        return new Fraction(this.numerator * other.denominator, this.denominator * other.numerator);
+    }
+
     @Override
     public String toString() {
         return "Fraction{" +
