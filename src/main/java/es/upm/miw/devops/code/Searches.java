9d1a26eb91ebb277234536b6990833e59040739a
DaniYDevs <danidzm18@gmail.com>
2025-09-25 17:07:47 +0200

    Added Search 6 issue #8

---
+++ src/main/java/es/upm/miw/devops/code/Searches.java
@@ -0,0 +1,32 @@
+package es.upm.miw.devops.code;
+
+import java.util.Objects;
+import java.util.stream.Stream;
+
+public class Searches {
+
+    /*public Stream<String> findFractionDivisionByUserId() {
+        return Stream.empty();
+    }*/
+
+    public Stream<Double> findDecimalImproperFractionByUserName(String name) {
+        UsersDatabase usersDatabase = new UsersDatabase();
+
+        return usersDatabase.findAll()
+                .filter(user -> user.getName().equals(name))
+                .flatMap(user -> user.getFractions().stream())
+                .filter(Fraction::isImproper)
+                .filter(f -> f.getDenominator() != 0) // avoid NaN/Infinity
+                .map(Fraction::decimal);
+    }
+
+    /*public Fraction findFirstProperFractionByUserId(String id) {
+        return null;
+    }*/
+
+
+    /*public Stream<String> findUserNameBySomeImproperFraction() {
+        return Stream.empty();
+    }*/
+
+}
