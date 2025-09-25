c32ca3d8e579bc6c231acfe225c56f4a949c9854
DaniYDevs <danidzm18@gmail.com>
2025-09-25 18:48:45 +0200

    Added Search 7 issue #10

--- src/main/java/es/upm/miw/devops/code/Searches.java
+++ src/main/java/es/upm/miw/devops/code/Searches.java
@@ -20,9 +20,16 @@ public class Searches {
                 .map(Fraction::decimal);
     }
 
-    /*public Fraction findFirstProperFractionByUserId(String id) {
-        return null;
-    }*/
+    public Fraction findFirstProperFractionByUserId(String id) {
+        UsersDatabase usersDatabase = new UsersDatabase();
+
+        return usersDatabase.findAll()
+                .filter(user -> user.getId().equals(id))
+                .flatMap(user -> user.getFractions().stream())
+                .filter(Fraction::isProper)
+                .findFirst()
+                .orElse(null); // chatGPT Line
+    }
 
 
     public Stream<String> findUserNameBySomeImproperFraction() {
